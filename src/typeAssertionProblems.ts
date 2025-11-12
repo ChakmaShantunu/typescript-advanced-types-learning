9173c4f3a84f8ab85106c614479262d30aff3379
shantunu007 <chakmashantunu.web@gmail.com>
2025-11-12 21:44:21 +0600

    convert num to number problem solve using type assertion

---
+++ src/typeAssertionProblems.ts
@@ -0,0 +1,14 @@
+// 👉 এটাকে number হিসেবে assert করে console এ num + 8 প্রিন্ট করো।
+
+let num: any = "42";
+let number = num as number;
+
+console.log(Number(number) + 8);
+
+// let random: unknown = "Shantunu Chakma";
+
+// // 👉 এটাকে string হিসেবে assert করে এর first 3 letter বের করো।
+
+// let user = (random as string).slice(0, 3);
+// console.log(user);
+
