3d550c7d817843f8ffcb4676f778ac5a2207bca3
shantunu007 <chakmashantunu.web@gmail.com>
2025-11-16 21:49:28 +0600

    add function and array typings using type and interface

--- src/typeInterface.ts
+++ src/typeInterface.ts
@@ -17,7 +17,7 @@ type Role = {
 
 type UserWithRole = User & Role
 
-interface IUserWithRole extends IUser{
+interface IUserWithRole extends IUser {
     role: 'admin' | 'user'
 }
 
@@ -29,5 +29,22 @@ const user1: IUserWithRole = {
 
 const user2: IUser = {
     name: "MRS. Supta",
-    age: 23, 
+    age: 23,
+}
+
+// interface using in function
+
+type Add = (num1: number, num2: number) => number
+
+interface IAdd {
+    (num1: number, num2: number) : number
+}
+
+const add: IAdd = (num1, num2) => num1 + num2
+
+type Friends = string[]
+const friends: Friends = ['a', 'b', 'c', 'd'];
+
+interface IFriends {
+    [index: number]: string
 }
