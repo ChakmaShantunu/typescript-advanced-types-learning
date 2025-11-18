f378f0ed47836a12df47dc3b1ffdf1754b8ed051
shantunu007 <chakmashantunu.web@gmail.com>
2025-11-18 22:02:20 +0600

    implemented two-parameter generic tuple type

--- src/generic.ts
+++ src/generic.ts
@@ -4,3 +4,8 @@ type GenericArray<T> = Array<T>
 const friends: GenericArray<string> = ['shantunu', 'supta', 'sadhana'];
 const rolleNumber: GenericArray<number> = [10, 20, 30];
 const isElligible: GenericArray<boolean> = [true, false, true];
+
+type Coordinates<x, y> = [x, y];
+const coordinates1: Coordinates<number, number> = [20, 30];
+const coordinates2: Coordinates<string, string> = ['20', '30'];
+const mixCoordinates: Coordinates<number, string> = [20, '30'];
