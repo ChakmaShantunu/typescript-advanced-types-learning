7945dd41076af1d96a310e790d0c6ec12d4ed2cd
shantunu007 <chakmashantunu.web@gmail.com>
2025-11-18 22:21:08 +0600

    implement GenericResponse with array-based data and meta types

--- src/generic.ts
+++ src/generic.ts
@@ -1,6 +1,7 @@
 
 type GenericArray<T> = Array<T>
 
+
 interface User {
     name: string;
     age: number;
@@ -17,3 +18,19 @@ type Coordinates<x, y> = [x, y];
 const coordinates1: Coordinates<number, number> = [20, 30];
 const coordinates2: Coordinates<string, string> = ['20', '30'];
 const mixCoordinates: Coordinates<number, string> = [20, '30'];
+
+
+type GenericResponse<T, M> = {
+    data: T;
+    meta: M;
+}
+
+const response: GenericResponse<User[], { requestedId: string }[]> = {
+    data: [
+        { name: 'shantunu', age: 30 },
+        { name: 'supta', age: 23 }
+    ],
+    meta: [
+        { requestedId: '12' }, { requestedId: '234' }
+    ]
+}
