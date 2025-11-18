14812521a88462d15556203bc8338a367a304945
shantunu007 <chakmashantunu.web@gmail.com>
2025-11-18 21:57:40 +0600

    implement GenericArray type with string, number, and boolean arrays

---
+++ src/generic.ts
@@ -0,0 +1,6 @@
+
+type GenericArray<T> = Array<T>
+
+const friends: GenericArray<string> = ['shantunu', 'supta', 'sadhana'];
+const rolleNumber: GenericArray<number> = [10, 20, 30];
+const isElligible: GenericArray<boolean> = [true, false, true];
